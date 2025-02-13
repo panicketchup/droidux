06dd52741e4851296333b1a2373e4f2eff2919f3
panicketchup <robertdebartolo@gmail.com>
2025-02-13 12:37:53 -0600

    Reapply "Put const clap in global"
    This reverts commit b3aed67dd43e7cb818d2094baf7139904001329a.

--- build.zig
+++ build.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const clap = @import("clap");
+
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
@@ -15,7 +17,7 @@ pub fn build(b: *std.Build) void {
 
     exe.pie = pie;
 
-    const clap = b.dependency("clap", .{});
+    //const clap = b.dependency("clap", .{});
     // exe.root_module.addImport("clap", clap.module("clap"));
     exe.addPackagePath("clap", "libs/zig-clap/clap.zig");
     // For uinput
